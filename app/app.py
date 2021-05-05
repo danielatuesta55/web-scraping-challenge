e63634b3c7db1eb567b74d541fe713c3f762be10
Daniel Atuesta <danielatuesta55@gmail.com>
2021-05-04 23:26:05 -0500

    added files to app.py and scrape.py

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_application")
 
 
 # Route to render index.html template using data from Mongo
@@ -16,8 +16,15 @@ def home():
     # Find one record of data from the mongo database
     mars_data = mongo.db.collection.find_one()
 
+    #make auto generated table a bootstrap style table
+    mars_data["Mars_Table"]=mars_data["Mars_Table"].replace('<table border="1" class="dataframe">',"<table class='table table-sm'>")
+    
+    print("--- MONGO DATA ---")
+    print(mars_data)
+    print("--- END MONGO DATA ---")
+
     # Return template and data
-    return render_template("index.html", data=mars_data)
+    return render_template("index.html", mission_mars=mars_data)
 
 
 
@@ -26,13 +33,13 @@ def home():
 def scrape():
     
     # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    return redirect("/",302)
 
 
 
--- app/scrape_mars.py
+++ app/scrape_mars.py
@@ -2,15 +2,20 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
+import urllib.request
+import requests
 from webdriver_manager.chrome import ChromeDriverManager
 
-def scrape_info():
+def init_browser():
     # Set up Splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
+    return browser
+def scrape():
+    browser = init_browser()
 
     # Visit https://mars.nasa.gov/news/
-    url = "https://mars.nasa.gov/news/"
+    url= "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     browser.visit(url)
     
     # Set time sleep for it not to crash 
@@ -21,25 +26,34 @@ def scrape_info():
     soup = bs(html, "html.parser")
 
     #News title
-    news_title = soup.find('div', 'class' == "content_title")
+    news_title = soup.find('div', class_= "content_title").text
 
     #News paragraph
-    news_p = soup.find('div','class' == "article_teaser_body")
+    news_p = soup.find('div', class_ = "article_teaser_body").text
 
     #featured_image_url 
-    featured_image_url = 'https://mars.nasa.gov/system/news_items/main_images/8936_First_selfie_animation_1200.jpg'
-    
+    # STUCK !!!
+    featured_image_url = 'https://mars.nasa.gov/#red_planet/0'
+    browser.visit(featured_image_url)
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
     #relative image path
     #<img alt="Perseverance's Selfie with Ingenuity" id="main_image" src="/system/news_items/main_images/8936_First_selfie_animation_1200.jpg">
-    relative_image_path = soup.find_all('img')[2]["src"]
+    image_name= soup.find('div', class_='image_container')
     mars_img = url + relative_image_path
 
     # Store data in a dictionary
     mars_data = {
-        "news_title": news_title,
-        "news_p": news_p,
-        "mars_img" : mars_img
-    }
+            "News_Title": news_title,
+            "Paragraph_Text": news_p,
+            "Most_Recent_Mars_Image": featured_image_url,
+            "Mars_Weather": mars_weather,
+            "Mars_Table": html_table,
+            "Mars_Hemispheres": hemisphere_img_urls
+        }
+
 
     # Close the browser after scraping
     browser.quit()
